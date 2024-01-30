9aceb74cdfd98eaddaa645a1c1461ec119635cde
senseiGai <swerss564@gmail.com>
2024-01-31 04:43:58 +0600

    versiom 1.1.5

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,6 +5,8 @@ import "../Footer/footer.scss";
 
 import footerlogo from "../../assets/footerlogo.svg";
 import sparklogo from "../../assets/sparklogo.svg";
+import sparklogofooter from "../../assets/sparklogofooter.svg";
+import logo from "../../assets/logo.svg";
 
 const Footer = () => {
   return (
@@ -30,6 +32,35 @@ const Footer = () => {
         </nav>
         <a href="https://sparkstudio.kz/" target="blank">
           <img src={sparklogo} alt="logo" className="footer_m__sparklogo" />
+        </a>
+      </section>
+      <div className="footer">
+        <img src={logo} alt="logo" className="footer__logo" />
+        <nav className="footer__nav">
+          <Link smooth to="main">
+            <h1 className="footer__text">Главная</h1>
+          </Link>
+          <Link smooth to="about-pc">
+            <h1 className="footer__text">О компании</h1>
+          </Link>
+          <Link smooth to="services">
+            <h1 className="footer__text">Услуги</h1>
+          </Link>
+          <Link smooth to="consultation">
+            <h1 className="footer__text">Контакты</h1>
+          </Link>
+          <Link smooth to="main">
+            <button className="footer__btn">Вверх</button>
+          </Link>
+        </nav>
+      </div>
+      <section className="footer__sparklogo">
+        <a href="https://sparkstudio.kz/" target="blank">
+          <img
+            src={sparklogofooter}
+            alt="logo"
+            className="footer__sparklogo-orange"
+          />
         </a>
       </section>
     </footer>
